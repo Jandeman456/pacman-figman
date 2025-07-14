38dbd947771d8bbeada8cf6951aa14e7090692e1
Jandeman456 <chrisjandobbelsteen@gmail.com>
2025-07-14 18:09:48 +0200

    Fix ESC functionality by removing separate function

--- public/pacman.js
+++ public/pacman.js
@@ -1158,6 +1158,9 @@ var PACMAN = (function () {
             document.getElementById("pacman").style.display = "none";
             document.getElementById("start-screen").style.display = "block";
             document.getElementById("leaderboard-button").style.display = "block";
+            document.getElementById("pacman").style.display = "none";
+            document.getElementById("start-screen").style.display = "block";
+            document.getElementById("leaderboard-button").style.display = "block";
             return false;
         } else if (e.keyCode === KEY.ESCAPE && state === LEADERBOARD) {
             setState(WAITING);
@@ -1201,13 +1204,6 @@ var PACMAN = (function () {
             startLevel();
         } else {
             showNameInput();
-        }
-    }
-
-    function setState(nState) { 
-        state = nState;
-        stateChanged = true;
-    }
     
     function collided(user, ghost) {
         return (Math.sqrt(Math.pow(ghost.x - user.x, 2) + 
